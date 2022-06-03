257367b45dcf0caae90d793c8142e88b89bbd520
Matheus H <matheushenriquedasilvaa.2021@gmail.com>
2022-06-03 15:48:09 -0300

    api created with success

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,6 +5,29 @@ const home = (request, h) => {
   return { msg: "Server On🔥" };
 };
 
+// find all users
+const findAllUsers = async (request, h) => {
+  try {
+    const users = await User.find();
+    h.response("ok").code(200);
+    return { users };
+  } catch (error) {
+    return { err: error };
+  }
+};
+
+// find user by id
+const findUserById = async (request, h) => {
+  const id = request.params.id;
+  try {
+    const user = await User.find({ _id: id });
+    h.response("ok").code(200);
+    return { user };
+  } catch (error) {
+    return { err: error };
+  }
+};
+
 // create user
 const createUser = async (request, h) => {
   const { name, email, password } = request.payload;
@@ -15,9 +38,54 @@ const createUser = async (request, h) => {
     password,
   };
 
+  const validation = await User.find({ email: user.email });
+
+  if (validation.length) {
+    return { err: "Email inválido!" };
+  }
+
   try {
     await User.create(user);
+    h.response("created").code(201);
     return { msg: "Usuário inserido no sistema com sucesso!" };
+  } catch (error) {
+    return { err: error };
+  }
+};
+
+// update user
+const updateUser = async (request, h) => {
+  const id = request.params.id;
+  const { name, email, password } = request.payload;
+
+  const user = {
+    name,
+    email,
+    password,
+  };
+
+  const validation = await User.find({ email: user.email });
+  if (validation.length && validation[0].email != user.email) {
+    return { err: "Email inválido!" };
+  }
+
+  try {
+    await User.findByIdAndUpdate({ _id: id }, { name, email, password });
+    h.response("created").code(201);
+    return { msg: "Usuário atualizado no sistema com sucesso!" };
+  } catch (error) {
+    return { err: error };
+  }
+};
+
+// delete user
+const deleteUser = async (request, h) => {
+  const id = request.params.id;
+
+  try {
+    await User.findByIdAndDelete({ _id: id });
+    h.response("delete").code(204);
+    return { msg: "Usuário deletado do sistema com sucesso!" };
   } catch (error) {
     return { err: error };
   }
@@ -26,4 +94,8 @@ const createUser = async (request, h) => {
 module.exports = {
   home,
   createUser,
+  findAllUsers,
+  findUserById,
+  updateUser,
+  deleteUser,
 };
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,9 +7,29 @@ const routes = [
     handler: controllers.home,
   },
   {
+    method: "GET",
+    path: "/users",
+    handler: controllers.findAllUsers,
+  },
+  {
+    method: "GET",
+    path: "/users/{id}",
+    handler: controllers.findUserById,
+  },
+  {
     method: "POST",
     path: "/users",
     handler: controllers.createUser,
+  },
+  {
+    method: "PUT",
+    path: "/users/{id}",
+    handler: controllers.updateUser,
+  },
+  {
+    method: "DELETE",
+    path: "/users/{id}",
+    handler: controllers.deleteUser,
   },
 ];
 
